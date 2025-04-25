f545a5511de55586dcae0eba21fe686ee4728958
MH <168056407+MinHeng1@users.noreply.github.com>
2025-04-26 01:22:39 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,10 +1,9 @@
-
-
 <?php
 session_start();
 $_SESSION['logout_success'] = true;
-session_unset();
 session_destroy();
+session_start(); // Restart session to store our success message
+$_SESSION['logout_success'] = true; // Set the success message again
 header("Location: home.php");
 exit();
 ?>
